1346567ff45370e7257481aa3e45252181296ec5
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-10-22 14:30:07 +0100

    Creada espectativa de mostrar fraccion como flotante

--- spec/racional_spec.rb
+++ spec/racional_spec.rb
@@ -17,9 +17,15 @@ describe Fraccion do
 		it " # Debe de estar en su forma reducida" do
 			@f2.to_s.should eq("3/7")
 		end
-		
+	end
+	describe " # Formateo de fracciones" do
 		it " # Se debe mostar por la consola la fraccion de la forma: a/b, donde a es el numerador y b el denominador" do
 			@f1.to_s.should == "3/4"
 		end
+		
+		it " # Se debe mostar por la consola la fraccion en formato flotante" do
+			@f1.to_f.should == "0.75"
+		end
+		
 	end
 end
