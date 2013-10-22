e68dde23ad683583eae7c1db1d276b38e769cf01
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-10-22 10:26:06 +0100

    Creada expectativa de que exista un numerador y se pueda acceder a el

--- spec/racional_spec.rb
+++ spec/racional_spec.rb
@@ -1,1 +1,9 @@
 require "lib/racional.rb"
+
+describe Fraccion do
+	describe " # Almacenamiento de los valores de numerador y denominador" do
+		it " # Debe existir un numerador y debe ser accesible por el metodo num()" do
+			@f1.num().should eq(3)
+		end
+	end
+end
