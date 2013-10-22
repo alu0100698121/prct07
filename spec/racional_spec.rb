d0a08bc51a291a5979ee59649fb8b1d0b99cab9d
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-10-22 10:48:53 +0100

    Creada espectativa de fraccion reducida

--- spec/racional_spec.rb
+++ spec/racional_spec.rb
@@ -3,6 +3,7 @@ require "lib/racional.rb"
 describe Fraccion do
 	before :each do
 		@f1 = Fraccion.new(3,4)
+		@f2 = Fraccion.new(6,14)
 	end
 	describe " # Almacenamiento de los valores de numerador y denominador" do
 		it " # Debe existir un numerador y debe ser accesible por el metodo num()" do
@@ -12,5 +13,10 @@ describe Fraccion do
 		it " # Debe existir un denominador y debe ser accesible por el metodo den()" do
 			@f1.den().should eq(4)
 		end
+		
+		it " # Debe de estar en su forma reducida" do
+			@f2.to_s.should eq("3/7")
+		end
+
 	end
 end
