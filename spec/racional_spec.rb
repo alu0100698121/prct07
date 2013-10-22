a57945947b32589108f975429ad55150ed13cc10
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-10-22 10:39:05 +0100

    Creada expectativa de que exista un denominador y se pueda acceder a el

--- spec/racional_spec.rb
+++ spec/racional_spec.rb
@@ -2,11 +2,15 @@ require "lib/racional.rb"
 
 describe Fraccion do
 	before :each do
-		@f1 = Fraccion.new(3)
+		@f1 = Fraccion.new(3,4)
 	end
 	describe " # Almacenamiento de los valores de numerador y denominador" do
 		it " # Debe existir un numerador y debe ser accesible por el metodo num()" do
 			@f1.num().should eq(3)
+		end
+		
+		it " #Debe existir un denominador y debe ser accesible por el metodo den()" do
+			@f1.den().should eq(4)
 		end
 	end
 end
